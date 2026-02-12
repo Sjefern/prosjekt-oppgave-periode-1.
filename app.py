0ca1df38de27d52dc05556e9a395dd3ad0e13719
Sjefern <sebastiankjos08@gmail.com>
2026-02-12 12:56:14 +0100

    lagt til navbar og basic styling i base som alle sidene arver, la også til stylingen til styling til en lenke, og la til rutene i py filen slik at lenkene funket.

--- app.py
+++ app.py
@@ -7,5 +7,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/handlekurv')
+def jandlekurv():
+    return render_template('handlekurv.html')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html')
+
+@app.route('/sokedatabase')
+def sokedatabase():
+    return render_template('sokedatabase.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/bestillinger')
+def bestillinger():
+    return render_template('bestillinger.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
