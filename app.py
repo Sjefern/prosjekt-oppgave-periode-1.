5e97d6a1aad6d73e0110278d2e164688bfd047b5
Sjefern <sebastiankjos08@gmail.com>
2026-02-12 14:13:00 +0100

    jeg har lagt til stiling til produktene som jeg selges på nettsiden. lagt til en liste med produktene som selges. gjort slik at tyggisene vises med navn, pris og bilde. har også gjort slik at man senere skal kunne trykke på den for mer innformasjon.

--- app.py
+++ app.py
@@ -3,13 +3,29 @@ import os
 
 app = Flask(__name__)
 
+
+PRODUCTS = [
+    {"id": 1, "name": "Mint Gum", "images": ["/static/img/Mintextra.png"], "price": 29, "description": "En frisk tyggegummi med sterk mintsmak."},
+    {"id": 2, "name": "Strawberry Gum", "images": ["/static/img/strawberryextra.png"], "price": 29, "description": "Søt og fruktig tyggegummi med jordbærsmak."},
+    {"id": 3, "name": "Blueberry Gum", "images": ["/static/img/blueberryextra.png"], "price": 29, "description": "En mild og behagelig tyggegummi med blåbærsmak."},
+    {"id": 4, "name": "Watermelon Gum", "images": ["/static/img/watermelonextra.png"], "price": 29, "description": "En saftig tyggegummi med smak av vannmelon."},
+    {"id": 5, "name": "Cinnamon Gum", "images": ["/static/img/cinnamonextra.png"], "price": 29, "description": "En varm og krydret tyggegummi med kanelpreg."},
+    {"id": 6, "name": "Mango Gum", "images": ["/static/img/mangoextra.png"], "price": 29, "description": "Eksotisk og søt tyggegummi med fyldig mangosmak."},
+    {"id": 7, "name": "Cola Gum", "images": ["/static/img/colaextra.png"], "price": 29, "description": "Klassisk tyggegummi med nostalgisk colasmak."},
+    {"id": 8, "name": "Bubblegum Gum", "images": ["/static/img/bubblegumextra.png"], "price": 29, "description": "Den originale bubblegum-smaken søt, myk og ikonisk."},
+    {"id": 9, "name": "Lakris Gum", "images": ["/static/img/lakrisextra.png"], "price": 29, "description": "Intens og karakteristisk tyggegummi med ekte lakrissmak."}
+]
+
+def find_product(product_id):
+    return next((p for p in PRODUCTS if p['id'] == product_id), None)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/products')
-def products():
-    return render_template('products.html')
+@app.route('/produkt')
+def produkt():
+    return render_template('product.html')
 
 @app.route('/handlekurv')
 def jandlekurv():
@@ -33,7 +49,7 @@ def bestillinger():
 
 @app.route('/tyggis')
 def tyggis():
-    return render_template('tyggis.html')
+    return render_template('tyggis.html', produkter=PRODUCTS)
 
 if __name__ == '__main__':
     app.run(debug=True)
